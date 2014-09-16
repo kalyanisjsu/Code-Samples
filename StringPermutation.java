e04025b7e84d40889eed9ca2b8adb5e22943e3b3
kalyanisjsu <kalyaninirmal6@gmail.com>
2014-09-16 11:38:01 -0700

    String permutation code
    Code for permutation of string

---
+++ StringPermutation.java
@@ -0,0 +1,25 @@
+
+public class StringPermutation {
+	
+	public void permutation(String str) { 
+	    permutation("", str); 
+	}
+
+	private void permutation(String prefix, String str) 
+	{
+	    int n = str.length();
+	    if (n == 0) 
+	    {
+	    	System.out.println(prefix);
+	    }
+	    else 
+	    {
+	        for (int i = 0; i < n; i++)
+	        {
+	        	 System.out.println("prefix : " + prefix + str.charAt(i) + "     str : "  +  str.substring(0, i) + str.substring(i+1, n));
+	        	 permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	        }
+	           
+	    }
+	}
+}
